a2a0f79733f770cd15fe7bc18aed9e373e1a5601
d-cab <diegocabral209@gmail.com>
2025-07-22 08:57:03 -0700

    Changed home page. Nav bar. Hero page

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("inquiryForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  
-  // Simple fake "submit"
-  const status = document.getElementById("formStatus");
-  status.textContent = "Thank you! We'll get back to you soon.";
-  this.reset();
-});
