c7f7022ede50852b0beb571449f8dafd3109086a
d-cab <diegocabral209@gmail.com>
2025-08-15 01:00:59 -0700

    Started implementing database, left with some bugs

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
 
-USERNAME = "admin"
-PASSWORD = "123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    passwordHash = db.Column(db.String(30), nullable=False)
+
+    def setPasswordHash(self,password):
+        self.passwordHash = generate_password_hash(password)
+
+    def checkPasswordHash(self, password):
+        return check_password_hash(self.passwordHash, password)
+
+user = User(username="admin")
+user.setPasswordHash("123")
 
 @app.route("/")
 def home():
@@ -17,23 +35,23 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-
-        if username == USERNAME and password == PASSWORD:
+        user = User.query.filter_by(username=username).first()
+        user.setPasswordHash(password)
+        if user and user.checkPasswordHash(password):
+            session['username'] = username
             return render_template("dashboard.html", username=username, password=password)
         else:
-            return render_template("login.html", error="Incorrect credentials")
+             return render_template("login.html", error="Incorrect credentials")
 
     else:
         return render_template("login.html")
 
-# @app.route("/dashboard")
-# def dashboard():
-#     return render_template("dashboard.html")
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
